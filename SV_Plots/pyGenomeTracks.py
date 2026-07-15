2eb20af09dd641e2d6ebe3186f708bd8037df46e
KemptonBryan <133163770+KemptonBryan@users.noreply.github.com>
2026-07-15 15:54:10 -0400

    Create pyGenomeTracks.py

---
+++ SV_Plots/pyGenomeTracks.py
@@ -0,0 +1,3 @@
+###I created a conda environment for pyGenomeTracks. Activate that environment then ran this. 
+
+pyGenomeTracks --tracks tracks_test.ini --region chr4_RagTag:174000000-185000000 --out BFB_variant_landscape_region.pdf
