afac3ce78f4451df9ea79bff96c606b2cc257c3c
KemptonBryan <133163770+KemptonBryan@users.noreply.github.com>
2026-07-23 17:49:54 -0400

    Delete SV_Plots directory

--- SV_Plots/pyGenomeTracks.py
+++
@@ -1,3 +0,0 @@
-###I created a conda environment for pyGenomeTracks. Activate that environment then ran this. 
-
-pyGenomeTracks --tracks tracks_test.ini --region chr4_RagTag:174000000-185000000 --out BFB_variant_landscape_region.pdf
